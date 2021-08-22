a388f83a70454187ca1a9d72ce51149b53ae0427
Victor Ghys <victor.ghys@student.howest.be>
2021-08-22 20:59:19 +0200

    added disaster and game mode

---
+++ Assets/Disaster.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Disaster : MonoBehaviour
+{
+    private DisasterType disasterType;
+
+    private enum DisasterType
+    {
+        Flood, Fire, Earthquake, Tornado, Winterstorm
+    }
+
+    private DisasterType GetRandomDisasterType()
+    {
+        Array values = Enum.GetValues(typeof(DisasterType));
+        DisasterType randomType = (DisasterType)values.GetValue(Random.Range(0, values.Length));
+        return randomType;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        disasterType = GetRandomDisasterType();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
---
+++ Assets/GameMode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMode : MonoBehaviour
+{
+    [SerializeField] private float earthRadius = 20;
+    [SerializeField] private Transform disasterPF;
+
+    private void CreateDisaster()
+    {
+        Vector3 pos = GetRandomDisasterSpawnPos();
+        Instantiate(disasterPF, pos, Quaternion.identity);
+    }
+
+    private Vector3 GetRandomDisasterSpawnPos()
+    {
+        Vector3 rand = Random.onUnitSphere;
+        Vector3 randPos = rand * earthRadius;
+        return randPos;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        CreateDisaster();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
